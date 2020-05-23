77651c87d147b20f5224e15dbdd95914805e3ee1
Lance Wicks <lw@judocoach.com>
2020-05-23 17:03:14 +0100

    Add ability to get contest info from API

--- lib/Webservice/Judobase/Contests.pm
+++ lib/Webservice/Judobase/Contests.pm
@@ -41,4 +41,30 @@ sub competition {
     return { error => 'Error retreiving competitor info' };
 }
 
+sub contest {
+    my ( $self, %args ) = @_;
+    return { error => 'id parameter is required' }
+      unless defined $args{id};
+
+    # https://data.ijf.org/api/get_json?access_token=&params[action]=contest.find&params[__ust]=&params[contest_code]=wc_sen2019_m_0060_0010&params[part]=info,score_list,media,events
+
+
+    my $url =
+        $self->url
+      . '?params[action]=contest.find'
+      . '&params[part]=info,score_list,media,events'
+      . '&params[contest_code]='
+      . $args{id};
+
+    my $ua = LWP::UserAgent->new;
+    my $request = HTTP::Request->new( GET => $url );
+
+    my $response = $ua->request($request);
+
+    return decode_json( $response->content )->{contests}
+      if $response->code == 200;
+
+    return { error => 'Error retreiving contest info' };
+}
+
 1;
